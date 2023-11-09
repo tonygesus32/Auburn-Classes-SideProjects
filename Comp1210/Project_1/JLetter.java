643fafaabc7642f3cb94d57106f2a91190ec5add
Anthony Tolbert <36686322+tonygesus32@users.noreply.github.com>
2023-11-08 18:36:11 -0600

    Add files via upload

---
+++ Comp1210/Project_1/JLetter.java
@@ -0,0 +1,33 @@
+/**
+* An application that displays letter J as a large block letter.
+* 
+* Project 1 - JLetter.java
+* @author Anthony Tolbert Comp 1210(003)
+* @version January 14, 2023
+**/
+
+public class JLetter
+{
+   /**
+   * Prints J,A,V,A to form the letter J.
+   * @param args Command line arguments - not used. 
+   **/
+
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("J     JAVA  ");
+      System.out.println("JA    JAVA  ");
+      System.out.println(" JAVAJAVA   ");
+      System.out.println("  JAVAJA    ");
+      
+   
+   
+   }
+
+}
---
+++ Comp1210/Project_1/MyLifeGoals.java
@@ -0,0 +1,35 @@
+/**
+* A simple program that will print out my short, medium, and long-term goals.
+*
+* Project 1 MyLifeGoals.java 
+* @author Anthony Tolbert Comp 1210(Section 003)
+* @version January 14, 2023
+*/
+
+public class MyLifeGoals
+{
+   /**
+    * Prints Name, short, medium and lon-term goals.
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) 
+   {
+      System.out.println("Anthony Tolbert");
+      System.out.println();
+      System.out.println("To get an A in this class and graduate from "
+               + "Auburn University. Also, land a succesful job so I can "
+               + "get an incredible job hopefully.");
+      System.out.println("To work at an awesome tech company, and "
+               + "constantly keep learning different languages so that I "
+               + "can continue to stay as well rounded and polished in "
+               + "coding as I can be.");
+      System.out.println("Start a family and become successful at my job. "
+               + "Also, travel the country and world accomplish everything "
+               + "that I have on my bucket list."); 
+                 
+      
+      
+      
+   }
+
+}
